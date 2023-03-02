1f776f5b16bf12cd7820d2a187b26ff58f375f07
Adegboyega Adedamola Wilson <wildam001@gmail.com>
2023-03-02 16:47:33 +0100

    first commit

---
+++ src/components/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ba70985e08mshe4561ea49192398p1534b8jsn7871f9c11a8b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "58a5ddf2756ec87753a43ef52690912d"
+
